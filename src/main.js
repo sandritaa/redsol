ff0f2912596e519347aef6a5f615e4aa28dfb284
taras_zhere <35704223+TarasZhere@users.noreply.github.com>
2021-08-20 18:53:11 -0700

    project setUp | layouts | router | elementUI | Vue Apollo | FakeQL

--- src/main.js
+++ src/main.js
@@ -1,6 +1,40 @@
-import { createApp } from 'vue'
+import { createApp, provide, h } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import installElementPlus from './plugins/element'
 
-createApp(App).use(store).use(router).mount('#app')
+/**
+ *  ADDING VUE APOLLO CLIENT
+ */
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+import { DefaultApolloClient } from '@vue/apollo-composable'
+
+// HTTP connection to the API
+const httpLink = createHttpLink({
+    // You should use an absolute URL here
+    uri: 'https://fakeql.com/graphql/7ca2730185a9cb33697083da57f41467',
+})
+
+// Cache implementation
+const cache = new InMemoryCache()
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+    link: httpLink,
+    cache,
+})
+
+
+const app = createApp({
+    setup () {
+        provide(DefaultApolloClient, apolloClient)
+    },
+
+    render: () => h(App),
+})
+
+installElementPlus(app)
+app.use(store).use(router)
+
+app.mount('#app')

407f73182a4af92e50a20be44403368cb1f03d20
taras_zhere <35704223+TarasZhere@users.noreply.github.com>
2021-10-15 13:33:22 -0500

    homepage layout set up

--- src/router/index.js
+++ src/router/index.js
@@ -1,65 +1,76 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
     {
         /**     NAMES AND PATHS
          *  - Always name your paths!
          */
-        path: '/',
-        name: 'Home',
+        path: "/",
+        name: "Home",
         /**     COMPONENTS VS COMPONENT
          *  - use COMPONENTS whene one of the page elements has to be injected automatically
-         *      (redirected || not redirected to another path, but redirected on the same path --> default), 
+         *      (redirected || not redirected to another path, but redirected on the same path --> default),
          *      or when you have multiple components
-         * 
+         *
          *  - use COMPONENT when you are sure there is not going to be more than one loaded component,
          *      and there is no redirection to that component
-         * 
+         *
          *  N.B.: if you are not sure which one to use, then always go for 'component' and NOT 'components'
          *      in order to use components you really need to know what you are using it for.
-         * 
+         *
          *  The case about is probably going to be the only case we need to use,
          *     so don't let yourself confuse you by it. Use: 'component: ()=>import([path...]) '
          */
-        components:{
-            default: ()=>import(/* webpackChunkName: "home" */ '../layouts/Home.vue')
+        components: {
+            // default: ()=>import(/* webpackChunkName: "home" */ '../layouts/Home.vue')
+            default: () =>
+                import(
+                    /* webpackChunkName: "home" */ "../layouts/HomePage.vue"
+                ),
         },
         /**     CHILDREN PATHS
          *  - Empty paths will be automaticall loaded
          *  - Remember that children is a array of objects
          */
-        children:[
+        children: [
             /**
              *  Atomatically inject the childcomponent if does not have a path
              */
             {
-                path:'',
-                name:'BackendTest',
-                component: ()=>import(/* webpackChunkName: "Backend" */ '../views/Backend.vue')
-            }
-        ]
+                path: "",
+                name: "BackendTest",
+                component: () =>
+                    import(
+                        /* webpackChunkName: "Backend" */ "../views/profileContent.vue"
+                    ),
+            },
+        ],
     },
     /**
      *  Profile layout used for both organization and usuario
      */
     {
-        path:'/profile',
-        name:'profile',
-        component: ()=>import(/* webpackChunkName: "profile" */ '../layouts/Profile.vue'),
-        redirect:'/profile',
-        children:[
+        path: "/profile",
+        name: "profile",
+        component: () =>
+            import(/* webpackChunkName: "profile" */ "../layouts/Profile.vue"),
+        redirect: "/profile",
+        children: [
             {
-                path:'',
-                name:'contentTest',
-                component:()=>import(/* webpackChunkName: "content" */ '../views/profileContent.vue')
-            }
-        ]
-    }
-]
+                path: "",
+                name: "contentTest",
+                component: () =>
+                    import(
+                        /* webpackChunkName: "content" */ "../views/profileContent.vue"
+                    ),
+            },
+        ],
+    },
+];
 
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
-    routes
-})
+    routes,
+});
 
-export default router
+export default router;
